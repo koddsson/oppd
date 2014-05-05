5b8b0088d7cee20b03463ced14b4f38774ad6ee6
Helgi Moller <helgi@azazo.com>
2014-05-05 15:21:23 +0000

    Unionizing 1. problem

--- helgimoller/euler.hs
+++ helgimoller/euler.hs
@@ -2,6 +2,6 @@ import Data.List
 -- Solution to ProjectEuler's 1. problem
 --
 one :: Int -> Int -> Int
-one x y = sum . nub $ concat [[0,x..999], [0,y..999]]
+one x y = sum $ [0,x..999] `union` [0,y..999]
 
 main = putStrLn . show $ one 3 5
