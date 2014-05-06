356e55574ed6da16842e0df75fc3373b36aad09c
Helgi Moller <helgi@azazo.com>
2014-05-06 19:39:39 +0000

    Code-cleanup.
    Removed an unnecessary function.

--- helgimoller/storecredit/storecredit.hs
+++ helgimoller/storecredit/storecredit.hs
@@ -1,12 +1,8 @@
-import System.Environment
-import Data.List
-import Data.Maybe
-import Text.Printf
-import System.IO
-import Control.Monad
-
-intread :: String -> Int
-intread = read
+import Data.List (zip)
+import Text.Printf (printf)
+import System.IO (readFile)
+import System.Environment (getArgs)
+import Control.Monad (replicateM)
 
 fborn :: (Int, Int, Int) -> Int
 fborn (a, b, c) = a
@@ -19,17 +15,15 @@ output cs (s, x', y') = do
 processCases :: [Int] -> [Int] -> IO ()
 processCases [] [] = putStrLn ""
 processCases (cs:css) (c:i:is) = do 
-                              let result = processCase c $ indexify $ (take i is)
+                              let result = processCase c $ replicateM 2 $ indexify (take i is)
                               putStrLn $ output cs $ head result
                               processCases css (drop i is)
 
 
-
-findElIndex :: Int -> [Int] -> Int
-findElIndex n lst = fromJust $ elemIndex n lst
-
-processCase :: Int -> [(Int, Int)] -> [(Int, Int, Int) ]
-processCase credit items = [(x+y, x', y') | [(x, x'),(y,y')] <- replicateM 2 items, (x+y) == credit, x' /= y']
+processCase :: Int -> [[(Int, Int)]] -> [(Int, Int, Int) ]
+processCase credit iitems = [
+                           (x+y, x', y') | [(x, x'),(y,y')] <- iitems,
+                           (x+y) == credit, x' /= y']
 
 indexify :: [Int] -> [(Int, Int)]
 indexify lst = zip lst [1..(length lst)] 
@@ -38,7 +32,7 @@ main :: IO()
 main = do
     f  <- getArgs
     contents <- readFile (f !! 0)
-    let lst = map intread . words $ contents
+    let lst = map read . words $ contents
     let ncase = head $ take 1 lst
-    let ss = drop 1 lst
-    processCases [1..ncase] ss
+    let cases = drop 1 lst
+    processCases [1..ncase] cases
